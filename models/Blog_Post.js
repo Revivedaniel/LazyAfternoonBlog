f4cf7f0c79949a386eb0a41f80023bfeb705cc4c
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-26 14:39:20 -0700

    Model defining a Blog_Post

---
+++ models/Blog_Post.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blog_Post extends Model {}
+
+Blog_Post.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  time_created: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  post: {
+      type: DataTypes.STRING,
+      allowNull: false
+  }
+});
