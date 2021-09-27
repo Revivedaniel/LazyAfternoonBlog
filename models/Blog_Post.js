4886021257109246b6c052fafaef8a97377d77c3
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-26 21:54:22 -0700

    Finished model

--- models/Blog_Post.js
+++ models/Blog_Post.js
@@ -3,24 +3,33 @@ const sequelize = require("../config/connection");
 
 class Blog_Post extends Model {}
 
-Blog_Post.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  time_created: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  post: {
+Blog_Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    time_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    post: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
   }
-});
+);
