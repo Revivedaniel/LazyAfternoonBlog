b9a23ba0a6e53fac48ba5b3e41399cf3b5455f25
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-05 21:36:46 -0700

    comments for style

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
-
+//Defining structure of the User table
 User.init(
   {
     id: {
@@ -24,6 +24,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      //validating that the data is in email format
       validate: {
         isEmail: true,
       },
@@ -31,6 +32,7 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      //Validating the data is greater than 8 in length
       validate: {
         len: [8],
       },
@@ -38,10 +40,12 @@ User.init(
   },
   {
     hooks: {
+      //Before creating a user, hash the password 10 times
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      //Before updating the user, hash the password 10 times
       beforeUpdate: async (updatedUserData) => {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
