9244a4a04a508438022f7856fdcdb955c20a7cb6
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-08 20:01:32 -0700

    C:/Program Files/Git/login page helpers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,3 +20,16 @@ router.get('/', async (req, res) => {
     res.status(500).json(err)
   }
 });
+
+//These routes help other routes redirect to /login with the auth middleware
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+router.put('/login', (req, res) => {
+  res.render('login');
+});
+router.delete('/login', (req, res) => {
+  res.render('login');
+});
+
+module.exports = router;
