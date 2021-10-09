4806372e835ee5650917b2e50a0399ad556d9a28
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-08 21:57:43 -0700

    Updating seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,15 @@
 const sequelize = require('../config/connection');
 const Blog_Post = require('../models/Blog_Post')
+const Comment = require('../models/Comment');
 
 const postData = require('./postData.json')
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const Blog_Posts = await Blog_Post.bulkCreate(postData);
+  const Comments = await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
