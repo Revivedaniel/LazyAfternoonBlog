766604da9430308379b8978099fa7f236e21876a
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-09 08:54:11 -0700

    posts and comments have user name

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const Comment = require('../models/Comment');
-const Blog_Post = require('../models/Blog_Post');
+const {User, Comment, Blog_Post} = require('../models')
+
 
 router.get('/', (req, res) => {
     res.redirect('/')
@@ -8,11 +8,26 @@ router.get('/', (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const postData = await Blog_Post.findByPk(req.params.id);
+        const postData = await Blog_Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
         const post = postData.get({plain: true})
-        const commentData = await Comment.findAll({where: {
-            post_id: req.params.id
-        }});
+        const commentData = await Comment.findAll({
+            where: {
+                post_id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
         const comments = commentData.map((c) => c.get({plain: true}))
         post.comments = comments
         res.json(post)
