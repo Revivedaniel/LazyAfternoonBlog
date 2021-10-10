f9a316544a1eed45ba2ec89e5bce7a0e42f034f8
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-10 06:29:45 -0700

    Post route for new comments

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Comment = require("../../models/comment");
+const withAuth = require("../../utils/auth")
+
+router.post("/", withAuth, async (req, res) => {
+    req.body.user_id = req.session.user_id
+    const newComment = {
+        post_id: req.body.postId,
+        user_id: req.session.user_id,
+        comment: req.body.comment
+    }
+    console.log(newComment)
+    try {
+        const commentData = await Comment.create(newComment)
+        res.status(200).json(commentData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+module.exports = router;
