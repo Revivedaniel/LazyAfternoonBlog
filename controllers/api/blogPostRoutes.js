ef0204891106f08123c6484a003f311b613c3e01
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-10 09:40:02 -0700

    Put route for updating posts

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -14,4 +14,23 @@ router.post("/", withAuth, async (req, res) => {
   } catch (err) {}
 });
 
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Blog_Post.findByPk(req.params.id);
+    const post = postData.get({ plain: true });
+    if (req.session.user_id == post.user_id) {
+      try {
+        const updatePost = await Blog_Post.update(req.body, {
+          where: { id: req.params.id },
+        });
+        res.status(200).json(updatePost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    }
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 module.exports = router;
