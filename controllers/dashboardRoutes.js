5c362888615912f962af585d385fe3b43d21f5ae
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-09 21:20:36 -0700

    Formatted name and date for post

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -6,9 +6,24 @@ router.get("/", async (req, res) => {
         const postData = await Blog_Post.findAll({
             where: {
                 user_id: req.session.user_id
-            }
+            },
+            include: [
+                {
+                  model: User,
+                  attributes: ["name"],
+                },
+              ],
+            order: [["id", "DESC"]],
         })
         const posts = postData.map((p) => p.get({ plain: true }))
+        for (let i = 0; i < posts.length; i++) {
+            const element = posts[i];
+            const date = new Date(element.time_created);
+            element.time_created = date.toLocaleDateString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+            });
+          }
         res.render('dashboard', {
             posts,
             id: req.session.user_id,
