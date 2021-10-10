d5e45ce9f2e550ec6c77a06583143cb8025c1712
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-09 20:27:50 -0700

    Added dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const postRoutes = require("./postRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 router.use("/", homeRoutes);
-router.use("/posts", postRoutes);
 router.use("/api", apiRoutes);
+router.use("/posts", postRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
