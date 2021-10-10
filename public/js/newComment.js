154f1d7b7d8f3136ef1e76df253384eaf2904818
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-10 06:30:10 -0700

    Public js for new comment

---
+++ public/js/newComment.js
@@ -0,0 +1,24 @@
+const comment_submit = document.querySelector("#comment_submit");
+
+comment_submit.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const postId = comment_submit.dataset.postid;
+  const comment = document.querySelector("#comment").value;
+  if (comment) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        postId,
+        comment,
+      }),
+      headers: {"Content-Type": "application/json"}
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+});
