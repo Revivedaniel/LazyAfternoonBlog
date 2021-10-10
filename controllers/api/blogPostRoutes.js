d35168fc75fd3b38b3d81a1accb09a313435dbf8
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-10 14:17:58 -0700

    Blog_Post delete route

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Blog_Post = require("../../models/Blog_Post");
+const { User, Comment, Blog_Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
@@ -35,13 +35,16 @@ router.put("/:id", withAuth, async (req, res) => {
 
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const postData = await Blog_Post.findByPk(req.params.id);
-    const post = postData.get({ plain: true });
+    const search = Number(req.params.id)
+    const post = await Blog_Post.findByPk(search);
     //Verifying that the user created this post
     if (req.session.user_id == post.user_id) {
       console.log("Success");
       try {
-        const post = await Blog_Post.destroy({ where: { id: req.params.id } });
+        const comments = await Comment.destroy({where: {post_id: search}})
+        const post = await Blog_Post.destroy(
+          { where: { id: search } }
+          );
         res.status(200).json(post);
       } catch (err) {
         res.status(404).json(err);
