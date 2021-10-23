070287414e25cd8ff2d1ac4ac53e59c86c2dc0d7
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-23 00:10:32 -0700

    Update button creates textarea

---
+++ patchNotes/styleUpdate1.md
@@ -0,0 +1,1 @@
+\+ Update comment button works now
--- public/js/editComment.js
+++ public/js/editComment.js
@@ -15,7 +15,15 @@ for (let i = 0; i < updateCommentButtonEl.length; i++) {
     cancelUpdateButtonEl[i].classList.remove("hidden");
     deleteCommentButtonEl[i].classList.add("hidden");
     updateCommentButtonEl[i].classList.add("hidden");
-    commentBodyEl[i].querySelector.classList.add("hidden");
+    commentBodyEl[i].classList.add("hidden");
+    const commentTextArea = document.createElement("textarea");
+    commentTextArea.setAttribute("id", "updatedComment");
+    console.log(event.target.parentElement.querySelector(".comment-text"))
+    commentTextArea.value = event.target.parentElement.querySelector(".comment-text").innerText;
+    commentTextArea.setAttribute("maxLength", "255");
+    commentTextArea.setAttribute("onfocus", 'this.style.height = "";this.style.height = this.scrollHeight + "px"');
+    commentTextArea.style.maxHeight = "100%";
+    event.target.parentElement.querySelector(".comment-body").appendChild(commentTextArea);
   });
 }
 
@@ -61,14 +69,14 @@ for (let i = 0; i < cancelUpdateButtonEl.length; i++) {
 for (let i = 0; i < saveCommentButtonEl.length; i++) {
   const element = saveCommentButtonEl[i];
   element.addEventListener("click", async (event) => {
+    const updatedComment = document.querySelector("#updatedComment").value;
     event.preventDefault();
     const response = await fetch(
       `/api/comments/${element.dataset.comment_id}`,
       {
         method: "PUT",
         body: JSON.stringify({
-          title,
-          post,
+          comment: updatedComment
         }),
         headers: { "Content-Type": "application/json" },
       }
