7257a48266cfe00708153079ea75e6bffcc803d9
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-30 19:00:37 -0700

    Validation returns to stop function

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -8,6 +8,7 @@ router.post("/", async (req, res) => {
     if (userData) {
       res.statusMessage = "Username must be unique";
       res.status(409).end();
+      return;
     }
   } catch (err) {}
   //check if email has been used
@@ -16,19 +17,23 @@ router.post("/", async (req, res) => {
     if (userData) {
       res.statusMessage = "Email must be unique";
       res.status(409).end();
+      return;
     }
   } catch (err) {}
   //use regex to verify email is email format
   const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
+  //min 8 chars, 1 or more special character, 1 or more numbers, 1 or more lowercase, 1 or more uppercase
+  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   if (!emailRegex.test(req.body.email)) {
         res.statusMessage = "Email not in correct format";
         res.status(409).end();
+        return;
       }
       //use regex to verify password has minimum requirements
       if (!passRegex.test(req.body.password)) {
         res.statusMessage = "Password not in correct format";
         res.status(409).end();
+        return;
       }
 
   try {
