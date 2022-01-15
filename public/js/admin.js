4092043a9fb701d4f4c829bf1dc972df70044d48
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-15 12:32:19 -0800

    Pretty

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,83 @@
-const postAdminButtons=document.querySelector("#adminButtons"),comments=document.querySelector("#comments"),deletePostConfirmationEl=document.querySelector("#adminDeleteConfirmation"),confirmPostButtonEl=document.querySelector("#adminConfirmButton"),cancelPostButtonEl=document.querySelector("#adminCancelButton");postAdminButtons&&postAdminButtons.addEventListener("click",async a=>{if(a.preventDefault(),a.target.matches("#adminDeleteButton")&&(a.target.classList.add("hidden"),deletePostConfirmationEl.classList.remove("hidden"),confirmPostButtonEl.classList.remove("hidden"),cancelPostButtonEl.classList.remove("hidden")),a.target.matches("#adminCancelButton")&&(a.preventDefault(),window.location.reload()),a.target.matches("#adminConfirmButton")){const b=await fetch(`/api/admin/posts/${a.target.dataset.postid}`,{method:"DELETE",body:"",headers:{"Content-Type":"application/json"}});b.ok?window.location.replace("/"):alert(b.statusText)}}),comments.addEventListener("click",async a=>{if(a.preventDefault(),a.target.matches(".adminCommentDeleteButton")){console.log(a.target.parentElement);const b=a.target.parentElement.querySelector(".adminDeleteCommentConfirmation"),c=a.target.parentElement.querySelector(".adminConfirmCommentDeleteButton"),d=a.target.parentElement.querySelector(".adminCancelUpdateButton");a.target.classList.add("hidden"),b.classList.remove("hidden"),c.classList.remove("hidden"),d.classList.remove("hidden")}if(a.target.matches(".adminCancelUpdateButton")&&(a.preventDefault(),window.location.reload()),a.target.matches(".adminConfirmCommentDeleteButton")){const b=await fetch(`/api/admin/comments/${a.target.dataset.comment_id}`,{method:"DELETE",body:"",headers:{"Content-Type":"application/json"}});b.ok?window.location.reload():alert(b.statusText)}});
+const postAdminButtons = document.querySelector("#adminButtons");
+const comments = document.querySelector("#comments");
+const deletePostConfirmationEl = document.querySelector(
+  "#adminDeleteConfirmation"
+);
+const confirmPostButtonEl = document.querySelector("#adminConfirmButton");
+const cancelPostButtonEl = document.querySelector("#adminCancelButton");
+if (postAdminButtons) {
+  postAdminButtons.addEventListener("click", async (event) => {
+    event.preventDefault();
+    //post admin delete button
+    if (event.target.matches("#adminDeleteButton")) {
+      event.target.classList.add("hidden");
+      deletePostConfirmationEl.classList.remove("hidden");
+      confirmPostButtonEl.classList.remove("hidden");
+      cancelPostButtonEl.classList.remove("hidden");
+    }
+    //post admin cancel button
+    if (event.target.matches("#adminCancelButton")) {
+      event.preventDefault();
+      window.location.reload();
+    }
+    //post admin confirm delete button
+    if (event.target.matches("#adminConfirmButton")) {
+      const response = await fetch(
+        `/api/admin/posts/${event.target.dataset.postid}`,
+        {
+          method: "DELETE",
+          body: "",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (response.ok) {
+        window.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  });
+}
+
+comments.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (event.target.matches(".adminCommentDeleteButton")) {
+    console.log(event.target.parentElement);
+    const commentDeleteConfirmationEl =
+      event.target.parentElement.querySelector(
+        ".adminDeleteCommentConfirmation"
+      );
+    const confirmCommentButtonEl = event.target.parentElement.querySelector(
+      ".adminConfirmCommentDeleteButton"
+    );
+    const cancelCommentButtonEl = event.target.parentElement.querySelector(
+      ".adminCancelUpdateButton"
+    );
+
+    event.target.classList.add("hidden");
+    commentDeleteConfirmationEl.classList.remove("hidden");
+    confirmCommentButtonEl.classList.remove("hidden");
+    cancelCommentButtonEl.classList.remove("hidden");
+  }
+
+  if (event.target.matches(".adminCancelUpdateButton")) {
+    event.preventDefault();
+    window.location.reload();
+  }
+
+  if (event.target.matches(".adminConfirmCommentDeleteButton")) {
+    const response = await fetch(
+      `/api/admin/comments/${event.target.dataset.comment_id}`,
+      {
+        method: "DELETE",
+        body: "",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+});
--- public/js/editComment.js
+++ public/js/editComment.js
@@ -1,1 +1,98 @@
-const updateCommentButtonEl=document.querySelectorAll(".commentUpdateButton"),deleteCommentButtonEl=document.querySelectorAll(".commentDeleteButton"),cancelUpdateButtonEl=document.querySelectorAll(".cancelUpdateButton"),saveCommentButtonEl=document.querySelectorAll(".saveCommentButton"),confirmCommentButtonEl=document.querySelectorAll(".confirmCommentButton"),commentBodyEl=document.querySelectorAll(".comment-text");for(let a=0;a<updateCommentButtonEl.length;a++){const b=updateCommentButtonEl[a];b.addEventListener("click",b=>{b.preventDefault(),saveCommentButtonEl[a].classList.remove("hidden"),cancelUpdateButtonEl[a].classList.remove("hidden"),deleteCommentButtonEl[a].classList.add("hidden"),updateCommentButtonEl[a].classList.add("hidden"),commentBodyEl[a].classList.add("hidden");const c=document.createElement("textarea");c.setAttribute("id","updatedComment"),console.log(b.target.parentElement.querySelector(".comment-text")),c.value=b.target.parentElement.parentElement.querySelector(".comment-text").innerText,c.setAttribute("maxLength","255"),c.setAttribute("onfocus","this.style.height = \"\";this.style.height = this.scrollHeight + \"px\""),c.style.maxHeight="100%",b.target.parentElement.parentElement.querySelector(".comment-body").appendChild(c)})}for(let a=0;a<deleteCommentButtonEl.length;a++){const b=deleteCommentButtonEl[a];b.addEventListener("click",b=>{b.preventDefault(),confirmCommentButtonEl[a].classList.remove("hidden"),cancelUpdateButtonEl[a].classList.remove("hidden"),updateCommentButtonEl[a].classList.add("hidden"),deleteCommentButtonEl[a].classList.add("hidden")})}for(let a=0;a<confirmCommentButtonEl.length;a++){const b=confirmCommentButtonEl[a];b.addEventListener("click",async a=>{a.preventDefault();const c=await fetch(`/api/comments/${b.dataset.comment_id}`,{method:"DELETE",body:"",headers:{"Content-Type":"application/json"}});c.ok?window.location.reload():alert(c.statusText)})}for(let a=0;a<cancelUpdateButtonEl.length;a++){const b=cancelUpdateButtonEl[a];b.addEventListener("click",a=>{a.preventDefault(),window.location.reload()})}for(let a=0;a<saveCommentButtonEl.length;a++){const b=saveCommentButtonEl[a];b.addEventListener("click",async a=>{const c=document.querySelector("#updatedComment").value;a.preventDefault();const d=await fetch(`/api/comments/${b.dataset.comment_id}`,{method:"PUT",body:JSON.stringify({comment:c}),headers:{"Content-Type":"application/json"}});d.ok?window.location.reload():alert(d.statusText)})}
+const updateCommentButtonEl = document.querySelectorAll(".commentUpdateButton");
+const deleteCommentButtonEl = document.querySelectorAll(".commentDeleteButton");
+const cancelUpdateButtonEl = document.querySelectorAll(".cancelUpdateButton");
+const saveCommentButtonEl = document.querySelectorAll(".saveCommentButton");
+const confirmCommentButtonEl = document.querySelectorAll(
+  ".confirmCommentButton"
+);
+const commentBodyEl = document.querySelectorAll(".comment-text");
+
+for (let i = 0; i < updateCommentButtonEl.length; i++) {
+  const element = updateCommentButtonEl[i];
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    saveCommentButtonEl[i].classList.remove("hidden");
+    cancelUpdateButtonEl[i].classList.remove("hidden");
+    deleteCommentButtonEl[i].classList.add("hidden");
+    updateCommentButtonEl[i].classList.add("hidden");
+    commentBodyEl[i].classList.add("hidden");
+    const commentTextArea = document.createElement("textarea");
+    commentTextArea.setAttribute("id", "updatedComment");
+    console.log(event.target.parentElement.querySelector(".comment-text"));
+    commentTextArea.value =
+      event.target.parentElement.parentElement.querySelector(
+        ".comment-text"
+      ).innerText;
+    commentTextArea.setAttribute("maxLength", "255");
+    commentTextArea.setAttribute(
+      "onfocus",
+      'this.style.height = "";this.style.height = this.scrollHeight + "px"'
+    );
+    commentTextArea.style.maxHeight = "100%";
+    event.target.parentElement.parentElement
+      .querySelector(".comment-body")
+      .appendChild(commentTextArea);
+  });
+}
+
+for (let i = 0; i < deleteCommentButtonEl.length; i++) {
+  const element = deleteCommentButtonEl[i];
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    confirmCommentButtonEl[i].classList.remove("hidden");
+    cancelUpdateButtonEl[i].classList.remove("hidden");
+    updateCommentButtonEl[i].classList.add("hidden");
+    deleteCommentButtonEl[i].classList.add("hidden");
+  });
+}
+
+for (let i = 0; i < confirmCommentButtonEl.length; i++) {
+  const element = confirmCommentButtonEl[i];
+  element.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      `/api/comments/${element.dataset.comment_id}`,
+      {
+        method: "DELETE",
+        body: "",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  });
+}
+
+for (let i = 0; i < cancelUpdateButtonEl.length; i++) {
+  const element = cancelUpdateButtonEl[i];
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.reload();
+  });
+}
+
+for (let i = 0; i < saveCommentButtonEl.length; i++) {
+  const element = saveCommentButtonEl[i];
+  element.addEventListener("click", async (event) => {
+    const updatedComment = document.querySelector("#updatedComment").value;
+    event.preventDefault();
+    const response = await fetch(
+      `/api/comments/${element.dataset.comment_id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          comment: updatedComment,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  });
+}
